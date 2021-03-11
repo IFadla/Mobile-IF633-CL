48c8f6220598801e5a15fc42339915b71f27d154
Fadla Ichsan <fadlaichsann@gmail.com>
2021-03-11 17:30:19 +0700

    list Music Done

--- Music_Player_App_UTS_32688/app/src/main/java/umn/ac/id/music_player_app_uts_32688/ListMusicActivity.java
+++ Music_Player_App_UTS_32688/app/src/main/java/umn/ac/id/music_player_app_uts_32688/ListMusicActivity.java
@@ -5,31 +5,153 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.Manifest;
 import android.annotation.SuppressLint;
+import android.content.Context;
 import android.content.Intent;
 import android.os.Bundle;
 import android.os.Environment;
 import android.view.Menu;
 import android.view.MenuInflater;
 import android.view.MenuItem;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.AdapterView;
 import android.widget.ArrayAdapter;
+import android.widget.BaseAdapter;
 import android.widget.ListView;
+import android.widget.TextView;
 
 import com.karumi.dexter.Dexter;
+import com.karumi.dexter.MultiplePermissionsReport;
 import com.karumi.dexter.PermissionToken;
 import com.karumi.dexter.listener.PermissionDeniedResponse;
 import com.karumi.dexter.listener.PermissionGrantedResponse;
 import com.karumi.dexter.listener.PermissionRequest;
+import com.karumi.dexter.listener.multi.MultiplePermissionsListener;
 import com.karumi.dexter.listener.single.PermissionListener;
 
 import java.io.File;
 import java.util.ArrayList;
+import java.util.List;
 
 public class ListMusicActivity extends AppCompatActivity {
-
+//    ListView listView;
+//    String[] items;
+    ListView myListViewForSongs;
+    String[] items;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_list_music);
+
+//        listView = findViewById(R.id.listViewSong);
+//
+//        runtimePermission();
+        myListViewForSongs = (ListView) findViewById(R.id.mySongListView);
+        runtimePermission();
+    }
+
+    public void runtimePermission() {
+        Dexter.withContext(this)
+                .withPermissions(Manifest.permission.READ_EXTERNAL_STORAGE, Manifest.permission.RECORD_AUDIO)
+                .withListener(new MultiplePermissionsListener() {
+                    @Override
+                    public void onPermissionsChecked(MultiplePermissionsReport multiplePermissionsReport) {
+                        display();
+                    }
+
+                    @Override
+                    public void onPermissionRationaleShouldBeShown(List<PermissionRequest> list, PermissionToken permissionToken) {
+                        permissionToken.continuePermissionRequest();
+                    }
+                }).check();
+//                .withListener(new PermissionListener() {
+//                    @Override
+//                    public void onPermissionGranted(PermissionGrantedResponse response) {
+//                        display();
+//                    }
+//
+//                    @Override
+//                    public void onPermissionDenied(PermissionDeniedResponse response) {
+//
+//                    }
+//
+//                    @Override
+//                    public void onPermissionRationaleShouldBeShown(PermissionRequest permissionRequest, PermissionToken token) {
+//                        token.continuePermissionRequest();
+//                    }
+//                }).check();
+    }
+
+    public ArrayList<File> findSong(File file) {
+        ArrayList<File> arrayList = new ArrayList<>();
+
+        File[] files = file.listFiles();
+
+        for (File singleFile: files) {
+            if (singleFile.isDirectory() && !singleFile.isHidden()) {
+                arrayList.addAll(findSong(singleFile));
+            }
+            else {
+                if (singleFile.getName().endsWith(".mp3") || singleFile.getName().endsWith(".wav")) {
+                    arrayList.add(singleFile);
+                }
+            }
+        }
+        return arrayList;
+    }
+
+    void display() {
+        final ArrayList<File> mySongs = findSong(Environment.getExternalStorageDirectory());
+
+        items = new String[mySongs.size()];
+
+        for (int i = 0; i < mySongs.size(); i++) {
+            items[i] = mySongs.get(i).getName().toString().replace(".mp3", "").replace(".wav", "");
+        }
+
+//        ArrayAdapter<String> myAdapter = new ArrayAdapter<>(this, android.R.layout.simple_list_item_1, items);
+//        myListViewForSongs.setAdapter(myAdapter);
+        customAdapter customAdapter = new customAdapter();
+        myListViewForSongs.setAdapter(customAdapter);
+
+        myListViewForSongs.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+            @Override
+            public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
+                String songName = (String) myListViewForSongs.getItemAtPosition(position);
+                startActivity(new Intent(getApplicationContext(), PlayerActivity.class)
+                        .putExtra("songs", mySongs)
+                        .putExtra("songname", songName)
+                        .putExtra("pos", position));
+            }
+        });
+    }
+
+    class customAdapter extends BaseAdapter {
+
+        @Override
+        public int getCount() {
+            return items.length;
+        }
+
+        @Override
+        public Object getItem(int position) {
+            return null;
+        }
+
+        @Override
+        public long getItemId(int position) {
+            return 0;
+        }
+
+        @Override
+        public View getView(int position, View convertView, ViewGroup parent) {
+            View myView = getLayoutInflater().inflate(R.layout.list_item, null);
+            TextView textSong = myView.findViewById(R.id.txtSongName);
+            textSong.setSelected(true);
+            textSong.setText(items[position]);
+
+            return myView;
+        }
     }
 
     @Override
@@ -53,7 +175,7 @@ public class ListMusicActivity extends AppCompatActivity {
     }
 
     private void moveToMyProfile() {
-        Intent intent = new Intent(this, ViewProfileActivity.class);
+        Intent intent = new Intent(this, PlayerActivity.class);
         startActivity(intent);
     }
 
@@ -61,4 +183,55 @@ public class ListMusicActivity extends AppCompatActivity {
         Intent intent = new Intent(this, MainActivity.class);
         startActivity(intent);
     }
+
+    // Permission file
+//    public void runtimePermission() {
+//        Dexter.withContext(this).withPermission(Manifest.permission.READ_EXTERNAL_STORAGE)
+//                .withListener(new PermissionListener() {
+//                    @Override
+//                    public void onPermissionGranted(PermissionGrantedResponse permissionGrantedResponse) {
+//                        displaySongs();
+//                    }
+//
+//                    @Override
+//                    public void onPermissionDenied(PermissionDeniedResponse permissionDeniedResponse) {
+//
+//                    }
+//
+//                    @Override
+//                    public void onPermissionRationaleShouldBeShown(PermissionRequest permissionRequest, PermissionToken permissionToken) {
+//                        permissionToken.continuePermissionRequest();
+//                    }
+//                }).check();
+//    }
+//
+//    public ArrayList<File> findSong (File file) {
+//        ArrayList<File> arrayList = new ArrayList<>();
+//
+//        File[] files = file.listFiles();
+//
+//        for (File singlefile: files) {
+//            if (singlefile.isDirectory() && !singlefile.isHidden()) {
+//                arrayList.addAll(findSong(singlefile));
+//            }
+//            else {
+//                if (singlefile.getName().endsWith(".mp3") || singlefile.getName().endsWith(".wab")) {
+//                    arrayList.add(singlefile);
+//                }
+//            }
+//        }
+//        return arrayList;
+//    }
+//
+//    void displaySongs() {
+//        final ArrayList<File> mySongs = findSong(Environment.getExternalStorageDirectory());
+//
+//        items = new String[mySongs.size()];
+//        for (int i = 0; i < mySongs.size(); i++) {
+//            items[1] = mySongs.get(i).getName().toString().replace(".mp3", "").replace(".wav", "");
+//
+//        }
+//        ArrayAdapter<String> myAdapter = new ArrayAdapter<String>(this, android.R.layout.simple_expandable_list_item_1, items);
+//        listView.setAdapter(myAdapter);
+//    }
 }
